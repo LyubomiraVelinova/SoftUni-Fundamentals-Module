7204049a6cfb3d261733e83cad468a41dde64c22
LyubomiraMihova <lyubomiramihova@gmail.com>
2022-12-10 22:42:17 +0200

    Add new file

---
+++ Python Fundamentals/04_Functions/Lab/05_calculate_rectangle_area.py
@@ -0,0 +1,8 @@
+def calculate_rectangle_area(width: int, height: int):
+    area = width * height
+    return area
+
+
+width = int(input())
+height = int(input())
+print(calculate_rectangle_area(width, height))
