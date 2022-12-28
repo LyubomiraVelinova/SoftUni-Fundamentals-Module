c7d4d327bfb741aee4c5d3a9fbf1bf512d27eb92
LyubomiraMihova <lyubomiramihova@gmail.com>
2022-12-28 21:57:58 +0200

    Delete file

--- Python Fundamentals/08_Text Processing/Lab/02_2var2.py
+++ Python Fundamentals/08_Text Processing/Lab/02_2var2.py
@@ -1,6 +1,6 @@
 text = input().split()
 new_word = ""
 for word in text:
-    new_word += word*len(word)
+    new_word += word * len(word)
 
 print(new_word)
--- Python Fundamentals/08_Text Processing/Lab/04_var2.py
+++
@@ -1,7 +0,0 @@
-from functools import reduce
-
-banned_words = input().split(", ")
-text = input()
-# text = reduce(lambda a,b: text.replace(a, "*"*len(a)))
-# ??? Try to do it!
-print(text)
